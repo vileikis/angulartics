12078c90b7ed216d58aa496f018488b4a565df71
Igor Vileikis <festys.rpo@gmail.com>
2014-09-15 14:17:38 +0300

    remove woopra idetify track code

--- src/angulartics-woopra.js
+++ src/angulartics-woopra.js
@@ -13,44 +13,23 @@
  */
 angular.module('angulartics.woopra', ['angulartics'])
 .config(['$analyticsProvider', function ($analyticsProvider) {
-    var isIdentified = false;
-    var eventList = [];
 
     $analyticsProvider.registerSetUserProperties(function (properties) {
-
       if (angular.isObject(properties) && angular.isString(properties.email)) {
         woopra.identify(properties);
-        isIdentified = true;
-
-        var delay = 1000;
-        angular.forEach(eventList, function (e) {
-
-          setTimeout(function () {
-            track(e.name, e.data);
-          }, delay);
-        });
-
       } else {
         throw "Invalid use properties or email. " + properties;
       }
     });
 
     $analyticsProvider.registerPageTrack(function (path) {
-      track('pv', {
+      woopra.track('pv', {
         url: path
       });
     });
 
     $analyticsProvider.registerEventTrack(function (action, properties) {
-      track(action, properties);
+      woopra.track(action, properties);
     });
-
-    function track (action, properties) {
-      if (!isIdentified) {
-        eventList.push({name: action, data: properties});
-      } else {
-        woopra.track(action, properties);
-      }
-    }
 }]);
 })(angular);
